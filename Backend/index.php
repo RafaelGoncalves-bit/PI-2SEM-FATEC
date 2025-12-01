5e71c42f958239b8590c9c2a91f6255f5817edc1
RafaelGonçalves <ltlrafaelg@gmail.com>
2025-11-30 21:50:11 -0300

    Testando rota

--- Backend/index.php
+++ Backend/index.php
@@ -1,12 +1,12 @@
 <?php
-require_once __DIR__ . '/public_html/g1/view/partials/header.php';
+require_once __DIR__ . '/view/partials/header.php';
 
-require_once __DIR__ . '/public_html/g1/view/partials/navbar.php';
+require_once __DIR__ . '/view/partials/navbar.php';
 ?>
 
 <h1>teste</h1>
 
 <?php 
-require_once __DIR__ . '/public_html/g1/view/partials/footer.php';
+require_once __DIR__ . '/view/partials/footer.php';
 
 ?>
