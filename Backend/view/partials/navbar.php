9fe6062e6ae04821fe26a120c540447ba75cb22b
RafaelGonçalves <ltlrafaelg@gmail.com>
2025-11-30 22:27:05 -0300

    Testando rota

--- Backend/view/partials/navbar.php
+++ Backend/view/partials/navbar.php
@@ -55,7 +55,7 @@ require_once __DIR__ . '/../../config/db.php';
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="<?= BASE_URL ?>/controller/OrcamentoController.php?acao=listar">Index</a></li>
-            <li><a class="dropdown-item" href="<?= BASE_URL ?>/controller/OrcamentoController.php?acao=novo">insert</a></li>
+            <li><a class="dropdown-item" href="<?= BASE_URL ?>/view/orcamento/new.php">insert</a></li>
           </ul>
         </li>
         <li class="nav-item dropdown">
